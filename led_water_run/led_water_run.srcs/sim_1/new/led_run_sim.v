e507b87256bafb9664cfc354d2c51871c3babc14
HIM <56875404+HIM@users.noreply.github.com>
2020-12-19 10:13:38 +0800

    第4次

--- led_water_run/led_water_run.srcs/sim_1/new/led_run_sim.v
+++ led_water_run/led_water_run.srcs/sim_1/new/led_run_sim.v
@@ -22,26 +22,25 @@
 
 module led_run_sim;
 
-    reg clk;        //输入reg，输出wire			reg：比特数据	wire：线连接
+    reg clk;     
     reg rst_n;
-    wire [3:0] led;		//输入wire类型，线连接
-        //做好连线准备
+    wire [3:0] led;		
+        
     led_run led_run_sim(
         .clk(clk), 
         .rst_n(rst_n), 
         .led(led)
     );
     
-    //激励		initial过程语句不带触发条件，其中语句序列只执行一次。常用于仿真模块中对激励向量的描述，是面向模拟仿真的过程语句
-    initial		//过程语句initial，中，必须对reg类型变量赋值	initial用于仿真中的初始化，只执行一次
+    //激励
+    initial
         begin
-            clk = 1;    //高
+            clk = 1;    
             rst_n = 0;
             #200
             rst_n = 1;  //200ns后置位
         end
         
-    //时钟周期20ns，10ns取反，50MHz时钟频率，
-    always #10 clk = ~clk;		//always中语句不断重复执行
+    always #10 clk = ~clk;		
 
 endmodule
--- led_water_run/led_water_run.srcs/sources_1/new/led_run.v
+++ led_water_run/led_water_run.srcs/sources_1/new/led_run.v
@@ -25,31 +25,7 @@ module led_run(clk, rst_n, led);
     input clk;
     input rst_n;
     output reg [3:0] led;
-    
-    reg [31:0] counter; //32位时钟
-    reg clk_1hz;
-    
-    parameter cnt_num = 4;
-/*
-    //对50MHz时钟分频，分到1Hz
-    always @ (posedge clk or negedge rst_n)			//()中写敏感信号表达式，即触发条件/时间表达式，当该表达式中变量值改变时，就会引发其中语句序列的执行		两个或以上敏感信号用or连接，
-    begin											//敏感信号：边沿敏感型对应时序电路，posedge、negedge两个关键字描述时钟信号	posedge表示时钟信号上升沿作为触发条件
-        if(!rst_n)
-            begin
-                counter <= 0;
-                clk_1hz <= 0;
-            end
-        else
-            if(counter < cnt_num)
-                counter <= counter + 1;
-            else
-                begin
-                    counter <= 0;
-                    clk_1hz <= ~clk_1hz;    //clk_1hz取反，就是1s时钟周期
-                end
-  
-    end
-*/
+     
     reg [1:0] state; //定义状态，状态机写流水灯
     
     always @ (posedge clk or negedge rst_n)
